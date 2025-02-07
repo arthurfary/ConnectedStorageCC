0f9d89ea055a9b9eb019a2a79830487cb9d1ce5c
Arthur Eduardo Fary <arthur@reveev.local>
2025-02-07 10:26:58 -0300

    starting menu

--- barrels.lua
+++ barrels.lua
@@ -4,6 +4,13 @@ local utils = require("utils")
 -- local chests = barrels.get_barrel_items_table()
 local item_sum = barrels.get_items_sum()
 
-utils.print_table(item_sum, true)
+-- Interface
+function menu()
+	local barrel_count = #barrels.get_barrels()
+	print("Welcome to Barrels")
+	print(barrel_count .. " Barrels detected.")
+end
 
+menu()
 
+-- utils.print_table(item_sum, true)
