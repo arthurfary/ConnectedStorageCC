8a688cf9a0b665c28aa864bdc09db325a1afa2c1
arthurfary <arthurfary22@gmail.com>
2026-01-01 14:05:59 -0300

    changes

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 return {
-    input_output_barrel_name = "minecraft:barrel_14", -- name of the input/output barrel
-    input_output_inventory_name = "minecraft:barrel_14" -- name of the input/output inventory
+  input_output_barrel_name = "sophisticatedstorage:barrel_0",     -- name of the input/output barrel
+  input_output_inventory_name = "sophisticatedstorage:barrel_0"   -- name of the input/output inventory
 }
